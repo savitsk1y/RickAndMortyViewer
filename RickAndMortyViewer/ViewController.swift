baa83dc454dca4359f30d6894735f6b2ab104717
Andrii.Savitskiy <Andrii.Savitskiy@tui.co.uk>
2025-04-26 23:26:19 +0300

    Initial Commit

---
+++ RickAndMortyViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyViewer
+//
+//  Created by Andrii Savytskyi on 26.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
